0fb9611d5bd6d579dc84b542fb77ee6ffaee5e8d
Maksim Veprev <maxveprevspb@gmail.com>
2022-10-17 09:58:55 +0100

    Add method to sort and calculate a median of a list

--- median.py
+++ median.py
@@ -1,12 +1,16 @@
 """Median calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
+
+def median(list):
+    temp = sorted(list)
+    mid = len(temp) // 2
+    return (temp[mid] + temp[-mid-1]) / 2
 
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        print(f"The median of this list is {median(numbers)}")
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
